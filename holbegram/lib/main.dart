aeaf950489f607842bc00be530ef4a5617805b82
CLMNTDFR <deferclement59@gmail.com>
2026-08-31 09:24:48 +0200

    add provider dependency and integrate ChangeNotifierProvider in main.dart; update auth_methods.dart with user profile loading functionality; enhance pubspec.lock with new package versions

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
+import 'providers/user_provider.dart';
 import 'screens/login_screen.dart';
 
 // App entry. Firebase has to be ready before we draw anything.
@@ -12,7 +14,7 @@ Future main() async {
   runApp(MyApp());
 }
 
-// Root widget. Starts on the login page for now.
+// root widget. starts on the login page for now.
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
@@ -21,19 +23,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Holbegram',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(218, 226, 37, 24),
-          brightness: Brightness.light,
-        ).copyWith(surface: Colors.white),
-      ),
-      home: LoginScreen(
-        emailController: emailController,
-        passwordController: passwordController,
+    return ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: MaterialApp(
+        title: 'Holbegram',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(218, 226, 37, 24),
+            brightness: Brightness.light,
+          ).copyWith(surface: Colors.white),
+        ),
+        home: LoginScreen(
+          emailController: emailController,
+          passwordController: passwordController,
+        ),
       ),
     );
   }
